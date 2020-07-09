32ee1804a2fb1c9c3631a6ebd9cd9eeaf38a3397
WeiLiang <kuahweiliang@hotmail.com>
2020-07-09 17:40:18 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('views/public', { extensions: ['html'] }));
+
+app.get('/random', (req, res) => {
+    res.send(Math.random());
+})
+
+let port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Frontend listening on port ${port}`));
